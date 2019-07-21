773a6bb6f2b226dca3ac4e54fe3b0ee7d9925e9c
DC <david112358c@gmail.com>
2019-07-21 21:48:52 +0800

    7/21 ||

---
+++ 309-Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int sold = 0;
+        int rest = 0;
+        int hold = INT_MIN;
+        for (const int price : prices) {
+            int prev_sold = sold;
+            sold = hold + price;
+            hold = max(hold, rest - price);
+            rest = max(rest, prev_sold);
+        }
+        return max(rest, sold);
+    }
+};
