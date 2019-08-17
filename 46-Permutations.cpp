4c46bc7095f65831ada653a3ef9cfcfbc10d3dbc
DC <david112358c@gmail.com>
2019-08-17 13:57:13 +0800

    8/17

--- 46-Permutations.cpp
+++ 46-Permutations.cpp
@@ -7,15 +7,13 @@ public:
 	}
 
 	void backtracking(vector<int>& nums, vector<vector<int>>& res,int begin) {
-		if (begin>=nums.size()) {
-			res.push_back(nums);
-		}
-		else {
-			for (int i = begin; i < nums.size(); ++i) {
-				swap(nums[begin], nums[i]);
-				backtracking(nums, res, begin + 1);
-				swap(nums[begin], nums[i]);
-			}
-		}
+		if(begin>=nums.size())res.push_back(nums);
+        else{
+            for(int i=begin; i<nums.size(); ++i){
+                swap(nums[begin], nums[i]);
+                backtracking(nums, res, begin+1);
+                swap(nums[begin], nums[i]);
+            }
+        }
     }
 };//tree traversal
